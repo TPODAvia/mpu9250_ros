7afd48bf7e297ec055589281b36778fb3f681bc7
TPODAvia <thepowerofdarknes2000@gmail.com>
2024-02-11 16:48:01 +0100

    update the rotations

--- src/mpu_node.py
+++ src/mpu_node.py
@@ -13,6 +13,7 @@ import argparse
 import math
 import tf2_ros
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 parser = argparse.ArgumentParser(description='My ROS Node with Argparse')
 parser.add_argument('calib_file', type=str, default='~/catkin_ws/src/OrcaRL/sensors/mpu9250_ros/config/calib.json', help='Calibration file')
@@ -128,9 +129,13 @@ def calculate_ned_frame(magnetic_field_vector, gravity_vector):
 
     return [down_vector, north_vector, east_vector]
 
-def calculate_quaternion_from_coordinates(initial_vector, ortogonal_vector1, ortogonal_vector2):
-    
-    desired_vector = [ortogonal_vector1[0]+ortogonal_vector2[1],ortogonal_vector1[1]-ortogonal_vector2[0],0]
+def calculate_quaternion_from_coordinates(initial_vector, ortogonal_vector1, ortogonal_vector2, gravity):
+
+    # print(f"{ortogonal_vector1} ::: {ortogonal_vector2}")
+    if gravity > 0:
+        desired_vector = [ortogonal_vector1[0]+ortogonal_vector2[1],ortogonal_vector1[1]-ortogonal_vector2[0],0]
+    else:
+        desired_vector = [ortogonal_vector1[0]-ortogonal_vector2[1],-ortogonal_vector1[1]-ortogonal_vector2[0],0]
     
     # Normalize the initial and final vectors
     initial_norm = initial_vector / np.linalg.norm(initial_vector)
@@ -138,6 +143,8 @@ def calculate_quaternion_from_coordinates(initial_vector, ortogonal_vector1, ort
     
     # Calculate the halfway vector
     halfway = final_norm + initial_norm
+
+    # print(f"{halfway} ::: {final_norm}")
     
     # Normalize the halfway vector
     halfway_norm = halfway / np.linalg.norm(halfway)
@@ -209,6 +216,57 @@ def estimage_yaw(yaw, gyro_z, dt):
     else:
         return yaw_est[0]
 
+def cancel_z_axis_rotation(quat):
+    """
+    Given a quaternion, this function cancels out the Z-axis rotation and returns the new quaternion.
+
+    :param quat: A numpy array representing the original quaternion [w, x, y, z]
+    :return: A numpy array representing the new quaternion with Z-axis rotation canceled out [w, x, y, z]
+    """
+    # Convert the quaternion to Euler angles
+    rotation = R.from_quat(quat)
+    euler_angles = rotation.as_euler('zyx', degrees=False)  # 'zyx' is for roll, pitch, yaw
+
+    # Set the Z component of the Euler angles to  0
+    euler_angles[2] =  0
+
+    # Convert the modified Euler angles back to a quaternion
+    new_rotation = R.from_euler('zyx', euler_angles, degrees=False)
+    new_quat = new_rotation.as_quat()
+
+    return new_quat
+
+def quaternion_to_yaw_quaternion(input_quat):
+    # Assuming input_quat is in the form [w, x, y, z]
+    # Calculate the yaw angle from the input quaternion
+    # yaw   = atan2(2.0 * (q.q3 * q.q0 + q.q1 * q.q2) , - 1.0 + 2.0 * (q.q0 * q.q0 + q.q1 * q.q1));
+    yaw_angle = math.atan2(2.0 * (input_quat[1] * input_quat[2] + input_quat[0] * input_quat[3]),  
+                            -1+2.0 * (input_quat[0] * input_quat[0] + input_quat[1] * input_quat[1]))
+    
+    # Convert the yaw angle to a quaternion
+    half_yaw =  - yaw_angle /  2.0
+
+    return [math.cos(half_yaw),  0, 0, math.sin(half_yaw)]
+    # return [1,  0, 0,  0]
+
+def quaternion_multiply(q1, q2):
+    """
+    Multiplies two quaternions.
+
+    :param q1: A  4-element array containing the first quaternion (q01, q11, q21, q31)
+    :param q2: A  4-element array containing the second quaternion (q02, q12, q22, q32)
+
+    :return: A  4-element array containing the final quaternion (q03, q13, q23, q33)
+    """
+    w0, x0, y0, z0 = q1
+    w1, x1, y1, z1 = q2
+
+    w = w0 * w1 - x0 * x1 - y0 * y1 - z0 * z1
+    x = w0 * x1 + x0 * w1 + y0 * z1 - z0 * y1
+    y = w0 * y1 - x0 * z1 + y0 * w1 + z0 * x1
+    z = w0 * z1 + x0 * y1 - y0 * x1 + z0 * w1
+
+    return [w, x, y, z]
 
 def talker():
 
@@ -231,13 +289,18 @@ def talker():
 
             # g_force = estimage_gravity([imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]], [imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]])
             projected_vec = calculate_ned_frame([imu.MagVals[0], imu.MagVals[1], imu.MagVals[2]], [imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]])
-            yaw_quaternion = calculate_quaternion_from_coordinates([1,0,0], projected_vec[1], projected_vec[2])
+            yaw_quaternion = calculate_quaternion_from_coordinates([1,0,0], projected_vec[1], projected_vec[2], imu.AccelVals[2])
             yaw_euler = quaternion_to_euler(yaw_quaternion)
             sensorfusion.updateRollAndPitch(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2], dt)
             comp_vel = speed_est.estimate_velocity(newTime, newTime-dt, imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2], sensorfusion.q)
             est_yaw = estimage_yaw(yaw_euler[2], imu.GyroVals[2], dt)
-            
-        quaternion_y = euler_to_quaternion(math.radians(sensorfusion.roll), math.radians(sensorfusion.pitch), math.radians(est_yaw))
+
+        quat0 = euler_to_quaternion(0, 0, -math.radians(est_yaw))
+        quat1 = quaternion_to_yaw_quaternion(sensorfusion.q)
+        quat2 = quaternion_multiply(quat1, sensorfusion.q)
+        quad3 = quaternion_multiply(quat0, quat2)
+
+        # print(f"{sensorfusion.roll} {sensorfusion.pitch} {est_yaw}")
 
         velo_msg = Twist()
         velo_msg.linear.x = comp_vel[0]
@@ -252,10 +315,10 @@ def talker():
         imu_msg.header.stamp = rospy.Time.now()
         imu_msg.header.frame_id = 'imu_link'
 
-        imu_msg.orientation.x = quaternion_y[1]
-        imu_msg.orientation.y = quaternion_y[2]
-        imu_msg.orientation.z = quaternion_y[3]
-        imu_msg.orientation.w = quaternion_y[0]
+        imu_msg.orientation.x = quad3[1]
+        imu_msg.orientation.y = quad3[2]
+        imu_msg.orientation.z = quad3[3]
+        imu_msg.orientation.w = quad3[0]
         imu_msg.angular_velocity.x = imu.GyroVals[0]
         imu_msg.angular_velocity.y = imu.GyroVals[1]
         imu_msg.angular_velocity.z = imu.GyroVals[2]
